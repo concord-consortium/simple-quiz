9bfb51e941b53b583a7e7005bf9bb45ba8dc847a
Scott Cytacki <scytacki@concord.org>
2013-04-10 16:02:49 -0400

    files from `rails new quiz`

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quiz::Application.config.secret_token = '36a3688863e18c7021dd8fa99dd5db555465877f031dbc76320b809d1fb40a827197c08d456acbc7096e931f4b164f4c13f60f207d1e095cb0c2912ef4175057'
